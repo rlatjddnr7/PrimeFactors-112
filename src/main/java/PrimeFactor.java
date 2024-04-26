b6cf4e0d8cc13c331862ca4dbfc2d73a796ef875
rlatjddnr7 <1993yso@gmail.com>
2024-04-26 16:58:49 +0900

    first commit

---
+++ src/main/java/PrimeFactor.java
@@ -0,0 +1,5 @@
+public class PrimeFactor {
+    public void test() {
+
+    }
+}
---
+++ src/test/java/PrimeFactorTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PrimeFactorTest {
+
+    @Test
+    void test1() {
+        PrimeFactor primeFactor = new PrimeFactor();
+
+    }
+}
