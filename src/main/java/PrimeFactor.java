c084594a470ebd6672fa72534793141cdb1b744c
rlatjddnr7 <1993yso@gmail.com>
2024-04-26 17:12:49 +0900

    first commit

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactor {
     public void test() {
 
+    }
+
+    public List<Integer> of(int i) {
+        List<Integer> factors = new ArrayList<>();
+        if (i==2) {
+            factors.add(2);
+        }
+        return factors;
     }
 }
--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,13 +1,23 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PrimeFactorTest {
 
     @Test
-    void test1() {
-        PrimeFactor primeFactor = new PrimeFactor();
+    public void test1() {
+        PrimeFactor primefactor = new PrimeFactor();
+        assertEquals(Arrays.asList(), primefactor.of(1));
 
+    }
+
+    @Test
+    public void testPrimefactorOf2() {
+        PrimeFactor primefactor = new PrimeFactor();
+        Assert.assertEquals(Arrays.asList(2), primefactor.of(2));
     }
 }
